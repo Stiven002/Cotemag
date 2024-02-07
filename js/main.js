5fb4a5028da146832514637babbaf2dd8ba6dd22
Stiven002 <stivendelgadoty@gmail.com>
2024-02-07 10:37:52 -0500

    Se agrego el modo responsive

--- js/main.js
+++ js/main.js
@@ -1,40 +1,62 @@
 // loader
-window.addEventListener('load', ()=>{
-  document.getElementById("loader").classList.toggle("loader__bg-2")
-})
+window.addEventListener("load", () => {
+  document.getElementById("loader").classList.toggle("loader__bg-2");
+});
 
 // seccion de lenguajes
-const lenguageButton = document.getElementById('lenguage'),
-    options = document.getElementById('btn-lenguage'),
-    option = document.getElementById('remove');
+const lenguageButton = document.getElementById("lenguage"),
+  options = document.getElementById("btn-lenguage"),
+  option = document.getElementById("remove");
 
-lenguageButton.addEventListener('click', () =>{
-    options.classList.toggle('lenguage-activate')
+lenguageButton.addEventListener("click", () => {
+  options.classList.toggle("lenguage-activate");
+});
+
+// menu responsive
+const openButton = document.getElementById("menu-open"),
+  menu = document.getElementById("menu"),
+  closeButton = document.getElementById("menu-close");
+
+openButton.addEventListener("click", () => {
+  menu.classList.add("menu-open");
+});
+closeButton.addEventListener("click", () => {
+  menu.classList.remove("menu-open");
 });
 
 // swiper del header
 var swiper = new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    effect: "fade",
-    loop: true,
-    navigation: {
-      nextEl: ".arrow-next",
-      prevEl: ".arrow-prev",
-    },
-    pagination: {
-      el: ".swiper__navegation-1",
-      clickable: true,
-    },
-  });
+  spaceBetween: 30,
+  effect: "fade",
+  loop: true,
+  navigation: {
+    nextEl: ".arrow-next",
+    prevEl: ".arrow-prev",
+  },
+  pagination: {
+    el: ".swiper__navegation-1",
+    clickable: true,
+  },
+});
+
 
 // swiper de los cursos
-  let swiperCourse = new Swiper(".mySwipere", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination-2",
-      clickable: true,
-      bulletActiveClass: "bullets-active",
+let swiperCourse = new Swiper(".mySwipere", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination-2",
+    clickable: true,
+    bulletActiveClass: "bullets-active",
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
     },
-  });
+
+    750: {
+      slidesPerView: 2,
+    }
+  }
+});
