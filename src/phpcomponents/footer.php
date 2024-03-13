d4a6c2f1b1f8e4d2736ff06f239f3b0bcba50f9f
Duvan Candelario <duvan.programador@gmail.com>
2024-03-13 17:29:54 -0500

    Creamos el componente del footer con todo lo relacionado al footer

---
+++ src/phpcomponents/footer.php
@@ -0,0 +1,63 @@
+<?php
+
+echo 
+'
+<footer class="footer basic__style">
+<div class="footer__container width__basic">
+    <div class="footer__contact">
+        <img src="./assets/img/Icons/LogoCotemag.png" loading="lazy" alt="Cotemag logo">
+        <div class="footer__tenic">
+            <div class="footer__soporte">
+                <h3>COTEMAG</h3>
+                <ul>
+                    <li><i class="fa-solid fa-phone"></i> (+57) 605-4231290 / 605-4230603</li>
+                    <li><a href="#"><i class="fa-brands fa-whatsapp"></i> (+57) 3004813444</a></li>
+                    <li><i class="fa-solid fa-location-dot"></i> Avenida Los Estudiantes #17 - 46</li>
+                    <li><i class="fa-solid fa-earth-americas"></i> Santa Marta - Colombia</li>
+                    <li><a href="#"><i class="fa-solid fa-envelope"></i> direcciongeneral@cotemag.edu.co</a></li>
+                </ul>
+            </div>
+            <div class="footer__soporte">
+                <h3>Ediciones</h3>
+                <ul>
+                    <li>
+                        <a href="#">Educacion Superior</a>
+                    </li>
+                    <li>
+                        <a href="#">Educacion para el trabajo</a>
+                    </li>
+                    <li>
+                        <a href="#">Colegios</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="footer__soporte">
+                <h3>Soporte Tecnico</h3>
+                <ul>
+                    <li>
+                        <a href="#">Contactenos</a>
+                    </li>
+                    <li>
+                        <a href="#">Pagos en linea</a>
+                    </li>
+                    <li>
+                        <a href="#">Acceso remoto</a>
+                    </li>
+                    <li>
+                        <a href="#">Empresa</a>
+                    </li>
+                    <li>
+                        <a href="#">Estado del servicio</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        
+    </div>
+    
+</div>
+</footer>
+'
+
+?>
+
