b7eb28b83263edc60d3284bc77465bf6c668fe1f
Duvan Candelario <duvan.programador@gmail.com>
2024-03-14 16:24:12 -0500

    Agregamos transiciones automaticas

--- js/main.js
+++ js/main.js
@@ -6,6 +6,9 @@ var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   effect: "fade",
   loop: true,
+  autoplay: {
+    delay: 3000, // Tiempo en milisegundos entre cada slide
+    disableOnInteraction: false,},
   navigation: {
     nextEl: ".arrow-next",
     prevEl: ".arrow-prev",
